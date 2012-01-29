554f3db8cc2a5c974b958c74e4333946ba91c097
defected <mike_ch_1@hotmail.com>
2012-01-28 21:39:12 -0500

    first commit

---
+++ TanitaScale/src/ca/charland/tanita/TanitaScaleActivity.java
@@ -0,0 +1,13 @@
+package ca.charland.tanita;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TanitaScaleActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
