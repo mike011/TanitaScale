c03c83c5258de57788cc58b4e0677060fecb3c1d
Michael Charland <mike_ch_1@hotmail.com>
2013-01-27 19:02:22 -0500

    new test files, no tests thou

---
+++ TanitaScaleTest/src/ca/charland/tanita/db/PersonDataHelperTest.java
@@ -0,0 +1,11 @@
+package ca.charland.tanita.db;
+
+import junit.framework.TestCase;
+
+public class PersonDataHelperTest extends TestCase {
+
+	public void testGetSex() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ TanitaScaleTest/src/ca/charland/tanita/db/TanitaDataHelperTest.java
@@ -0,0 +1,19 @@
+package ca.charland.tanita.db;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TanitaDataHelperTest {
+
+	@Test
+	public void testGetPrevious() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAverage() {
+		fail("Not yet implemented");
+	}
+
+}
