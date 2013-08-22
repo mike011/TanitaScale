4606f66215a34f3d1baca67cf9c9322c2670959a
Michael Charland <mike_ch_1@hotmail.com>
2013-08-21 22:56:15 -0400

    fix crash on first attempt for a new user to add new data

--- TanitaScale/src/ca/charland/tanita/db/TanitaDataHelper.java
+++ TanitaScale/src/ca/charland/tanita/db/TanitaDataHelper.java
@@ -8,22 +8,25 @@ public class TanitaDataHelper {
 
 	public static double getPrevious(List<Data> tanitaData, TanitaDataTable.Column column) {
 		double prev = 0;
-		if (!tanitaData.isEmpty()) {
-			TanitaData td = (TanitaData) tanitaData.get(tanitaData.size() - 2);
+		int index = tanitaData.size() - 2;
+		if (index >= 0) {
+			TanitaData td = (TanitaData) tanitaData.get(index);
 			prev = td.get(column);
 		}
 		return prev;
 	}
 
 	public static double getAverage(List<Data> tanitaData, TanitaDataTable.Column column) {
-		double all = 0;
-		for (Data d : tanitaData) {
-			TanitaData td = (TanitaData) d;
-			all += td.get(column);
-		}
-		double avg = 0;
-		if (all != 0) {
-			avg = all / tanitaData.size() ;
+		double avg = 0;		
+		if (!tanitaData.isEmpty()) {
+			double all = 0;
+			for (Data d : tanitaData) {
+				TanitaData td = (TanitaData) d;
+				all += td.get(column);
+			}
+			if (all != 0) {
+				avg = all / tanitaData.size();
+			}
 		}
 		return avg;
 	}
